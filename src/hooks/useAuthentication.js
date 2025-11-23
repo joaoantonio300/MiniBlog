bfb025a47d7ca8d35eba388c7e15ec8d8db015a2
João Antonio <134206664+joaoantonio300@users.noreply.github.com>
2025-11-23 20:50:23 -0300

    Update useAuthentication.js

--- src/hooks/useAuthentication.js
+++ src/hooks/useAuthentication.js
@@ -1,9 +1,5 @@
-// mesmo nao fazendo uso do "db" eu preciso exportar as confs do meu banco no firebase
 import {db, auth} from "../firebase/config"
 
-// lembrar que sempre que eu for referncia ao firebase eu tenho que chamar o arquivo aonde eu armazenei as conf dele
-// como o arquivo ai encima
-//ok
 import {
     getAuth,
     createUserWithEmailAndPassword,
@@ -15,43 +11,30 @@ import {
 import {useState, useEffect} from 'react'
  
 export const useAuthentication = () => {
-    // ok
     const [error, setError] = useState(null);
     const [loading, setLoading] = useState(null);
 
-    // cleanup
-    // deal with memory leak
-    // menos sapoha aqui 
     const [cancelled, setCancelled] = useState(false);
 
-    // checkando se foi cancelado
     function checkIfIsCancelled() {
         if (cancelled) {
             return;
         }
     }
-
-    // criando usuario
      
     const createUser = async (data) => {
 
-    // everything before the try are just validations
         checkIfIsCancelled()
 
         setLoading(true)
         setError(null);
 
-        try {
-    // aqui eu puxo o user de uma função que é importada do firebase
-    // a primeira coisa é passar a auth, junto com o email e password que foi a forma que nosso banco está estruturado
-    
+        try {  
             const {user} = await createUserWithEmailAndPassword(
                 auth, 
                 data.email,
                 data.password
             )
-
-    // para salvar outras informações como nome, precisamos atualizar o usuário criado;
             await updateProfile(user, {
                 displayName: data.displayName
             })
@@ -62,7 +45,6 @@ export const useAuthentication = () => {
 
         } catch (error) {
 
-            // procurar como funciona mais a questão dos tratamentos de erros aqui 
             console.log(error.message)
             console.log(typeof error.message)
 
@@ -83,13 +65,9 @@ export const useAuthentication = () => {
 
     }
 
-    // logout - sign out
     const logout = () => {
-
         checkIfIsCancelled();
-
         signOut(auth);
-
     }
 
     // login - sign in 
@@ -106,7 +84,6 @@ export const useAuthentication = () => {
             setLoading(false)
              
         } catch (error) {
-
             let systemErrorMessage;
 
             if (error.code.includes("invalid-credential")) {
